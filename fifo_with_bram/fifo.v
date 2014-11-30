874d0851afe1d73eec739fec1177c56c4e9ef5b2
monotone-RK <kobaya4121@gmail.com>
2014-11-30 22:48:34 +0900

    re_tmp was replaced with dout_en

--- fifo_with_bram/fifo.v
+++ fifo_with_bram/fifo.v
@@ -48,76 +48,74 @@ module test;
 
   always @(posedge CLK) begin
     if (!RST_X) begin
-	 cycle <= 0;
+      cycle <= 0;
     end else begin
-	 cycle <= cycle + 1;
-	 $write("cycle(DIN): %d DOUT: %d ENQ: %d DEQ: %d EMPTY %d FULL %d\n", 
-		   cycle, DOUT, ENQ, DEQ, EMPTY, FULL);
-	 if (cycle == `HALT_CYCLE) $finish;
+      cycle <= cycle + 1;
+      $write("cycle(DIN): %d DOUT: %d ENQ: %d DEQ: %d EMPTY %d FULL %d\n", 
+             cycle, DOUT, ENQ, DEQ, EMPTY, FULL);
+      if (cycle == `HALT_CYCLE) $finish;
     end
   end
 endmodule
 
-module FIFO(input  wire             CLK, 
-            input  wire             RST_X, 
-            input  wire             ENQ, 
-            input  wire             DEQ, 
-            input  wire [WIDTH-1:0] DIN, 
-            output wire [WIDTH-1:0] DOUT, 
-            output wire             EMPTY, 
-            output wire             FULL);
-  
-  parameter WIDTH = 32; // data width
-  parameter DEPTH = 4;  // fifo depth
-  parameter W_CNT = 3;  // counter width
-  parameter W_POS = 2;  // pointer width for DEPTH
+module FIFO #(parameter               WIDTH = 32, // data width
+              parameter               DEPTH = 4,  // fifo depth
+              parameter               W_CNT = 3,  // counter width
+              parameter               W_POS = 2)  // pointer width for DEPTH
+             (input  wire             CLK, 
+              input  wire             RST_X, 
+              input  wire             ENQ, 
+              input  wire             DEQ, 
+              input  wire [WIDTH-1:0] DIN, 
+              output wire [WIDTH-1:0] DOUT, 
+              output wire             EMPTY, 
+              output wire             FULL);
   
   reg [W_CNT-1:0] cnt;
   reg [W_POS-1:0] head;
   reg [W_POS-1:0] tail;
-  reg             re_tmp;
-
+  reg             dout_en;
+  
   wire             re = DEQ && !EMPTY;
   wire             we = ENQ && !FULL;
-  wire [WIDTH-1:0] data_out;
-	  
+  wire [WIDTH-1:0] dout;
+  
   FIFO_RAM #(.WIDTH(WIDTH), .DEPTH(DEPTH), .W_POS(W_POS))
-  fifo_ram(CLK, we, head, tail, DIN, data_out);
-
-  assign DOUT  = (re_tmp) ? data_out : 0;
+  fifo_ram(CLK, we, head, tail, DIN, dout);
+  
+  assign DOUT  = (dout_en) ? dout : 0;
   assign EMPTY = (cnt == 0);
   assign FULL  = (cnt == DEPTH);
 
   always @(posedge CLK) begin
     if (!RST_X) begin
-	 cnt    <= 0;
-	 head   <= 0;
-	 tail   <= 0;
-	 re_tmp <= 0;
+      cnt    <= 0;
+      head   <= 0;
+      tail   <= 0;
+      dout_en <= 0;
     end else begin
-	 if (we) tail <= (tail == DEPTH-1) ? 0 : tail+1;
-	 if (re) head <= (head == DEPTH-1) ? 0 : head+1;
-	 if (we && re) cnt <= cnt;
-	 else if (we)  cnt <= cnt + 1;
-	 else if (re)  cnt <= cnt - 1;
-	 re_tmp <= re;
+      if (we) tail <= (tail == DEPTH-1) ? 0 : tail+1;
+      if (re) head <= (head == DEPTH-1) ? 0 : head+1;
+      if (we && re) cnt <= cnt;
+      else if (we)  cnt <= cnt + 1;
+      else if (re)  cnt <= cnt - 1;
+      dout_en <= re;
     end
   end
 endmodule
 
-module FIFO_RAM(input  wire             CLK,
-                input  wire             WE,
-                input  wire [W_POS-1:0] RADDR,
-                input  wire [W_POS-1:0] WADDR,
-                input  wire [WIDTH-1:0] DIN,
-                output reg  [WIDTH-1:0] DOUT);
+module FIFO_RAM #(parameter               WIDTH = 32, // data width
+                  parameter               DEPTH = 4,  // fifo depth
+                  parameter               W_POS = 2)  // pointer width for DEPTH
+                 (input  wire             CLK,
+                  input  wire             WE,
+                  input  wire [W_POS-1:0] RADDR,
+                  input  wire [W_POS-1:0] WADDR,
+                  input  wire [WIDTH-1:0] DIN,
+                  output reg  [WIDTH-1:0] DOUT);
   
-  parameter WIDTH = 32; // data width
-  parameter DEPTH = 4;  // fifo depth
-  parameter W_POS = 2;  // pointer width for DEPTH
-
   reg [WIDTH-1:0] mem [DEPTH-1:0];
-
+  
   always @(posedge CLK) begin
     if (WE) mem[WADDR] <= DIN;
     DOUT <= mem[RADDR];
